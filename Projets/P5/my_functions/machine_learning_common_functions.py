251cc61a2e3bbbe29608e9ac475c6b17dd7ab3ba
DrHadiNono <el-hadi.nono@hotmail.fr>
2022-05-01 19:53:00 +0200

    p5 v0 Data Merge + Explo

---
+++ Projets/P5/my_functions/machine_learning_common_functions.py
@@ -0,0 +1,197 @@
+from sklearn.model_selection import train_test_split
+from sklearn import metrics, dummy
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.inspection import permutation_importance
+from scipy.cluster.hierarchy import dendrogram
+
+from my_functions.common_functions import *
+from matplotlib import cm
+
+import timeit
+
+
+def plot_dendrogram(Z, names):
+    plt.figure(figsize=(10, 25))
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('distance')
+    dendrogram(
+        Z,
+        labels=names,
+        orientation='left',
+    )
+    plt.show()
+
+
+def trainning_sets(data, Y, train_size=0.8, random_state=None, shuffle=True, sampling_factor=1, scale=None, scale_y=False):
+    if sampling_factor < 1:
+        data_size = len(data)
+        sample_size = int(data_size*sampling_factor)
+        # print('Original data size:', data_size, 'Sample data size:', sample_size)
+
+        sample = np.random.choice(data_size, size=sample_size, replace=False)
+        data = data.iloc[sample]
+
+    cols = data.columns.tolist()
+    cols.remove(Y)
+
+    X = data[cols]
+    Ycol = Y
+
+    if scale_y:
+        Y = data[[Ycol]]
+        if scale == 'std':
+            Y = Std_Scaled(Y)[:, 0]
+        if scale == 'min-max':
+            Y = MinMax_Scaled(Y)[:, 0]
+        if scale == 'robust':
+            Y = Robust_Scaled(Y)[:, 0]
+        if scale == 'power':
+            Y = PowerTransformer_Scaled(Y)[:, 0]
+    else:
+        Y = data[Y].values
+
+    # Data Split 5 Trainning sets
+    xtrain, xtest, ytrain, ytest = train_test_split(
+        X, Y, train_size=train_size, random_state=random_state, shuffle=shuffle)
+
+    # Data Scaling. Training and Test sets scaled one after another to avoid data leakage
+    if scale == 'std':
+        xtrain = Std_Scaled(xtrain)
+        xtest = Std_Scaled(xtest)
+    if scale == 'min-max':
+        xtrain = MinMax_Scaled(xtrain)
+        xtest = MinMax_Scaled(xtest)
+    if scale == 'robust':
+        xtrain = Robust_Scaled(xtrain)
+        xtest = Robust_Scaled(xtest)
+    if scale == 'power':
+        xtrain = PowerTransformer_Scaled(xtrain)
+        xtest = PowerTransformer_Scaled(xtest)
+
+    return xtrain, xtest, ytrain, ytest
+
+
+def train(X_train, y_train, X_test, y_test, model_name, model, Perfs, scores=None, hue=None):
+    print('--------------------', model_name, '--------------------')
+
+    start_time = timeit.default_timer()
+    # Entraînement
+    model.fit(X_train, y_train)
+    # Prédiction sur le jeu de test
+    y_pred = model.predict(X_test)
+    elapsed = timeit.default_timer() - start_time
+
+    i = len(Perfs)
+    Perfs.loc[i, 'Model'] = model_name
+
+    # Evaluatation
+    if scores == None:
+        scores = ['RMSE', 'R²']
+    if 'RMSE' in scores:
+        RMSE = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+        Perfs.loc[i, 'RMSE'] = RMSE
+    if 'RMSLE' in scores:
+        y_pred[y_pred < 0] = 0
+        RMSLE = np.sqrt(metrics.mean_squared_log_error(y_test, y_pred))
+        Perfs.loc[i, 'RMSLE'] = RMSLE
+    if 'R²' in scores:
+        R2 = np.abs(metrics.r2_score(y_test, y_pred))
+        if R2 > 1:
+            R2 = 0
+        Perfs.loc[i, 'R²'] = R2
+    Perfs.loc[i, 'Time(s)'] = elapsed
+    if hue != None:
+        Perfs.loc[i, 'hue'] = hue
+    # Convertion de type
+    for score in scores:
+        Perfs[score] = Perfs[score].astype('float')
+    print(Perfs.loc[i])
+
+    # afficher les prédictions
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.scatterplot(x=y_test, y=y_pred, color='coral',
+                    s=20, label='Actual Model', ax=ax)
+    sns.lineplot(x=[min(y_test), max(y_test)], y=[
+                 min(y_test), max(y_test)], color='gray', label='Ideal Model', ax=ax)
+    # étiqueter les axes et le graphique
+    plt.xlabel('Target', fontsize=16)
+    plt.ylabel('Prediction', fontsize=16)
+    plt.title(
+        model_name + ' (R²=' + str(round(Perfs.loc[i, 'R²'], 3)) + ')', fontsize=20)
+    plt.show()
+    return Perfs, model
+
+
+def train_cv(X_train, y_train, X_test, y_test, model_name, model, Perfs, param_grid, score_cv='neg_mean_squared_log_error', cv=5, scores=None, hue=None):
+    # Créer un classifieur kNN avec recherche d'hyperparamètre par validation croisée
+    cv_model = RandomizedSearchCV(
+        model,  # un classifieur kNN
+        param_grid,     # hyperparamètres à tester
+        cv=cv,           # nombre de folds de validation croisée
+        scoring=score_cv,   # score à optimiser
+        n_jobs=-1
+    )
+
+    perf, model = train(X_train, y_train, X_test, y_test,
+                        model_name, cv_model, Perfs, scores, hue)
+    print("Meilleur(s) hyperparamètre(s) sur le jeu d'entraînement:",
+          cv_model.best_params_)
+    return perf, cv_model.best_estimator_
+
+
+def display_scores(Perfs, y, yloc=0.92, hue=None):
+    Perfs = Perfs.copy()
+    scores = Perfs.columns.tolist()
+    scores.remove('Model')
+    scores.remove('hue')
+    if hue != None:
+        Perfs = renameCol(Perfs.copy(), 'hue', hue)
+    Perfs[colsOfType(Perfs)] = round(Perfs[colsOfType(Perfs)], 3)
+
+    nb_line = len(colsOfType(Perfs))
+    nb_col = 1
+    height = nb_line * (3.3 if hue == None else 3)
+    width = Perfs.shape[0] * (1.5 if hue == None else 0.7)
+    wspace = 0.05
+    hspace = 0.3
+
+    # Préparation de l'affichage des graphiques sur deux colonnes : une pour les histogrammes et une pour les boxplots
+    fig, axes = plt.subplots(nb_line, nb_col, figsize=(
+        width, height), sharex=False, sharey=False)
+    # ajuster l'espace entre les graphiques.
+    fig.subplots_adjust(wspace=wspace, hspace=hspace)
+    fig.suptitle('Evaluation (' + y + ')', x=0.5, y=yloc, fontsize=24,
+                 horizontalalignment='center')  # Titre globale de la figure
+
+    for i in range(0, len(scores)):
+        ax = axes[i]
+        sns.barplot(data=Perfs, ax=ax, x=Perfs['Model'], y=scores[i], hue=hue)
+        for i in ax.containers:
+            ax.bar_label(i,)
+        ax.set_xticklabels(ax.get_xticklabels(),
+                           rotation=(8 if hue == None else 5))
+        ax.set_xlabel('', fontsize=0)
+    plt.show()
+
+
+def features_importances(X, y, X_test, y_test, model):
+    feature_names = X.columns.tolist()
+    feature_names.remove(y)
+
+    result = permutation_importance(
+        model, X_test, y_test, n_repeats=10, random_state=0, n_jobs=-1
+    )
+    forest_importances = pd.DataFrame(
+        result.importances_mean, index=feature_names, columns=['RMAD'])
+    forest_importances['RMAD'] = forest_importances['RMAD'].apply(
+        lambda x: np.sqrt(x) if x >= 0 else 0)
+
+    fig, ax = plt.subplots(figsize=(10, int(len(feature_names))*0.4))
+    sns.barplot(data=forest_importances, y=feature_names,
+                x='RMAD', orient='h', ax=ax)
+    for i in ax.containers:
+        ax.bar_label(i,)
+    ax.set_title(
+        "Feature importances (" + y + ")", fontsize=20)
+    ax.set_xlabel("Root Mean Accuracy Decrease")
+    plt.show()
---
+++ Projets/P5/my_functions/pca_functions.py
@@ -0,0 +1,194 @@
+from .common_functions import *
+
+from sklearn.impute import KNNImputer
+from sklearn import decomposition
+
+from matplotlib.collections import LineCollection
+
+
+def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None):
+    for (d1, d2) in axis_ranks:  # On affiche les n_comp/2 premiers plans factoriels,
+        if d2 < n_comp:
+            # initialisation de la figure
+            fig, ax = plt.subplots(figsize=(7, 6))
+
+            # détermination des limites du graphique
+            if lims is not None:
+                xmin, xmax, ymin, ymax = lims
+            elif pcs.shape[1] < 30:
+                xmin, xmax, ymin, ymax = -1, 1, -1, 1
+            else:
+                xmin, xmax, ymin, ymax = min(pcs[d1, :]), max(
+                    pcs[d1, :]), min(pcs[d2, :]), max(pcs[d2, :])
+
+            # affichage des flèches
+            # s'il y a plus de 30 flèches, on n'affiche pas le triangle à leur extrémité
+            if pcs.shape[1] < 30:
+                plt.quiver(np.zeros(pcs.shape[1]), np.zeros(pcs.shape[1]),
+                           pcs[d1, :], pcs[d2, :],
+                           angles='xy', scale_units='xy', scale=1, color='grey')
+                # (voir la doc : https://matplotlib.org/api/_as_gen/matplotlib.pyplot.quiver.html)
+            else:
+                lines = [[[0, 0], [x, y]] for x, y in pcs[[d1, d2]].T]
+                ax.add_collection(LineCollection(
+                    lines, axes=ax, alpha=.1, color='black'))
+
+            # affichage des noms des variables
+            if labels is not None:
+                for i, (x, y) in enumerate(pcs[[d1, d2]].T):
+                    if x >= xmin and x <= xmax and y >= ymin and y <= ymax:
+                        plt.text(x, y, labels[i], fontsize='14', ha='center',
+                                 va='center', rotation=label_rotation, color='blue', alpha=0.5)
+
+            # affichage du cercle
+            circle = plt.Circle((0, 0), 1, facecolor='none', edgecolor='b')
+            plt.gca().add_artist(circle)
+
+            # définition des limites du graphique
+            plt.xlim(xmin, xmax)
+            plt.ylim(ymin, ymax)
+
+            # affichage des lignes horizontales et verticales
+            plt.plot([-1, 1], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-1, 1], color='grey', ls='--')
+
+            # nom des axes, avec le pourcentage d'inertie expliqué
+            plt.xlabel('F{} ({}%)'.format(
+                d1+1, round(100*pca.explained_variance_ratio_[d1], 1)))
+            plt.ylabel('F{} ({}%)'.format(
+                d2+1, round(100*pca.explained_variance_ratio_[d2], 1)))
+
+            plt.title('Cercle des corrélations (F{} et F{})'.format(d1+1, d2+1))
+            plt.show(block=False)
+
+
+def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, continuous_illustrative_var=None, discrete_illustrative_var=None):
+    if continuous_illustrative_var is not None:
+        # Generate a custom diverging colormap
+        cmap = sns.color_palette('RdYlGn_r', as_cmap=True)
+
+    if discrete_illustrative_var is not None:
+        title = discrete_illustrative_var.name
+
+    for (d1, d2) in axis_ranks:  # On affiche les n_comp/2 premiers plans factoriels,
+        if d2 < n_comp:
+            # initialisation de la figure
+            fig, ax = plt.subplots(figsize=(7, 6))
+
+            # affichage des points
+            if discrete_illustrative_var is None and continuous_illustrative_var is None:
+                plt.scatter(X_projected[:, d1],
+                            X_projected[:, d2], alpha=alpha)
+
+            if continuous_illustrative_var is not None:
+                plt.scatter(X_projected[:, d1],
+                            X_projected[:, d2], c=continuous_illustrative_var, cmap=cmap, alpha=alpha)
+                plt.colorbar(label=continuous_illustrative_var.name)
+
+            if discrete_illustrative_var is not None:
+                discrete_illustrative_var = np.array(
+                    discrete_illustrative_var)
+                for value in np.unique(discrete_illustrative_var):
+                    selected = np.where(discrete_illustrative_var == value)
+                    plt.scatter(
+                        X_projected[selected, d1], X_projected[selected, d2], alpha=alpha, label=value)
+                plt.legend(title=title)
+
+            # affichage des labels des points
+            if labels is not None:
+                for i, (x, y) in enumerate(X_projected[:, [d1, d2]]):
+                    plt.text(x, y, labels[i], fontsize='14',
+                             ha='center', va='center')
+
+            # détermination des limites du graphique
+            plt.xlim([np.min(X_projected[:, [d1]])*1.1,
+                     np.max(X_projected[:, [d1]])*1.1])
+            plt.ylim([np.min(X_projected[:, [d2]])*1.1,
+                     np.max(X_projected[:, [d2]])*1.1])
+
+            # affichage des lignes horizontales et verticales
+            plt.plot([-100, 100], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-100, 100], color='grey', ls='--')
+
+            # nom des axes, avec le pourcentage d'inertie expliqué
+            plt.xlabel('F{} ({}%)'.format(
+                d1+1, round(100*pca.explained_variance_ratio_[d1], 1)))
+            plt.ylabel('F{} ({}%)'.format(
+                d2+1, round(100*pca.explained_variance_ratio_[d2], 1)))
+
+            plt.title(
+                'Projection des individus (sur F{} et F{})'.format(d1+1, d2+1))
+            plt.show(block=False)
+
+
+def display_scree_plot(pca):
+    scree = pca.explained_variance_ratio_*100
+    plt.bar(np.arange(len(scree))+1, scree)
+    plt.plot(np.arange(len(scree))+1, scree.cumsum(), c='red', marker='o')
+    plt.xlabel("rang de l'axe d'inertie")
+    plt.ylabel("pourcentage d'inertie")
+    plt.title('Eboulis des valeurs propres')
+    plt.show(block=False)
+
+
+def PCA(data, n_comp=5, cols=None, alpha=1, continuous_illustrative_var=None, discrete_illustrative_var=None, enable_display_scree_plot=True, enable_display_circles=True, enable_display_factorial_planes=True):
+    ''' Calcul et affiche l'ACP d'un data set '''
+    if cols is None:
+        cols = data.columns.tolist()
+
+    # choix du nombre de composantes à calculer
+    n_comp = min(n_comp, len(cols))
+    Fs = [(i, i+1) for i in range(0, n_comp, 2)]
+
+    data_pca = data[cols]
+
+    # préparation des données pour l'ACP
+    if data_pca.isnull().sum().sum() > 0:
+        knn_imputer = KNNImputer(n_neighbors=max(
+            10, int(len(data_pca)*0.1)), weights='distance')
+        knn_imputer_transfom = knn_imputer.fit_transform(data_pca)
+        data_pca = pd.DataFrame(knn_imputer_transfom,
+                                columns=data_pca.columns, index=data_pca.index)
+
+    X = data_pca.values
+    names = data.index  # ou data.index pour avoir les intitulés
+    features = data[cols].columns
+
+    # Centrage et Réduction
+    X_scaled = Std_Scaled(X)
+
+    # Calcul des composantes principales
+    pca = decomposition.PCA(n_components=n_comp)
+    pca.fit(X_scaled)
+
+    if enable_display_scree_plot:
+        # Eboulis des valeurs propres
+        display_scree_plot(pca)
+
+    pcs = pca.components_
+    if enable_display_circles:
+        # Cercle des corrélations
+        display_circles(pcs, n_comp, pca, Fs, labels=np.array(features))
+
+    if enable_display_factorial_planes:
+        # Projection des individus
+        X_projected = pca.transform(X_scaled)
+        # , labels = np.array(names))
+        display_factorial_planes(X_projected, n_comp, pca,
+                                 Fs, alpha=alpha, continuous_illustrative_var=continuous_illustrative_var, discrete_illustrative_var=discrete_illustrative_var)
+
+    return pcs
+
+
+def PCA_Compression(data, components, cols=None, prefix='comp'):
+    ''' Applique la réduction dimensionnelle sur les colonnes d'un data set avec les composantes ACP en paramètre '''
+    if cols == None:
+        cols = data.columns.tolist()
+
+    compressed_data = {}
+    for i in range(len(components)):
+        compressed_data[prefix+str(i+1)] = sum([data[cols[j]]
+                                                * components[i][j] for j in range(len(components[i]))])
+
+    compressed_data = pd.DataFrame(compressed_data, index=data.index)
+    return compressed_data
