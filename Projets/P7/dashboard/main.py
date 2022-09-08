ae44e8975e377c58e40d0b37d096ca9e01c19bc3
DrHadiNono <el-hadi.nono@hotmail.fr>
2022-09-08 02:07:35 +0200

    p7 v3.1 dashboard expanders

--- Projets/P7/dashboard/main.py
+++ Projets/P7/dashboard/main.py
@@ -46,6 +46,28 @@ async def post(session, url, data):
         return {}
 
 
+@cached(ttl=None, cache=Cache.MEMORY)
+async def get_data():
+    async with aiohttp.ClientSession() as session:
+        data = await fetch(session, dashboard_url+'data')
+        df = pd.DataFrame.from_dict(data, orient='tight')
+        for col in list(df):
+            if df[col].max() == 1:
+                df[col] = df[col].astype(int)
+        return df
+
+
+@cached(ttl=None, cache=Cache.MEMORY)
+async def get_model():
+    async with aiohttp.ClientSession() as session:
+        model = await fetch(session, dashboard_url+'model')
+        if model:
+            model = dillDecode(model)
+        else:
+            st.error(model)
+        return model
+
+
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
     Adds a UI on top of a dataframe to let viewers filter columns
@@ -123,25 +145,20 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def filter_viz(df: pd.DataFrame):
-    display = st.checkbox("Distributions")
-
-    if display:
+    with st.expander("Distributions"):
         modification_container = st.container()
         with modification_container:
             to_filter_columns = st.multiselect("Colonnes", df.columns)
 
             if len(to_filter_columns) >= 1:
-                print(to_filter_columns)
                 st.pyplot(histo_distribution(df[to_filter_columns]))
 
 
 def compare_viz(df: pd.DataFrame):
-    compare = st.checkbox("Comparaison")
-
-    if compare:
+    with st.expander("Comparaison avec les autres clients"):
         modification_container = st.container()
         with modification_container:
-            to_filter_columns = st.multiselect("Colonnes", df.columns)
+            to_filter_columns = st.multiselect('', df.columns)
             if len(to_filter_columns) > 2:
                 st.warning(
                     'Uniquement les deux premières colonnes sont prises en compte.', icon="⚠️")
@@ -158,15 +175,13 @@ def display_data(df):
 
 @cached(ttl=None, cache=Cache.MEMORY)
 async def applicant_data(applicant):
-    st.write('## Client Sélectionné')
     st.dataframe(applicant, width=1500)
 
 
 @cached(ttl=None, cache=Cache.MEMORY)
 async def prediction(applicant, nb_features_shap, _explainer, _model):
     applicant = applicant.drop(columns='SK_ID_CURR')
-    st.write('## Prediction')
-    st.write('#### Probabilité de défaut de paiment')
+    st.write('## Probabilité de défaut de paiment')
     async with aiohttp.ClientSession() as session:
         score = await post(session, dashboard_url+'scoreApplicant', {'applicant': dillEncode(applicant.to_dict('records')[0])})
         if score:
@@ -183,90 +198,74 @@ async def prediction(applicant, nb_features_shap, _explainer, _model):
             st.metric('', score_text, delta=str(
                 round((score-st.session_state.previous_score)*100, 2))+'%')
         st.session_state.previous_score = score
-        st.write('#### Détails')
-        shap_explanation = _explainer(applicant)[0]
-        st.pyplot(shap.plots.waterfall(
-            shap_explanation, max_display=min(nb_features_shap+1, len(list(applicant)))))
+
+        with st.expander("Détails", False):
+            shap_explanation = _explainer(applicant)[0]
+            st.pyplot(shap.plots.waterfall(
+                shap_explanation, max_display=min(nb_features_shap+1, len(list(applicant)))))
 
 
+# ========================================================================================================================
 async def main(model, explainer, df):
     st.set_option('deprecation.showPyplotGlobalUse', False)
 
     st.write("# Dashboard Prêt à dépenser")
     st.write('## Clients')
-    df_filtred = filter_dataframe(df)
-    st.dataframe(df_filtred, width=1500)
+    with st.expander("Liste", True):
+        df_filtred = filter_dataframe(df)
+        st.dataframe(df_filtred, width=1500)
     filter_viz(df_filtred)
 
-    # Inputs to the sidebar
-    st.sidebar.write('# Sélection Client')
-    settings_form = st.sidebar.form(key='settings_form')
+    with st.sidebar:
+        # Inputs to the sidebar
+        st.write('# Sélection Client')
+        with st.form(key='settings_form'):
+            # Applicants IDs
+            ids = df_filtred['SK_ID_CURR'].values.tolist()
+            id = st.selectbox(label='ID client', options=ids)
+            idx = ids.index(id)
 
-    # Applicants IDs
-    ids = df_filtred['SK_ID_CURR'].values.tolist()
-    id = settings_form.selectbox(label='ID client', options=ids)
-    idx = ids.index(id)
-
-    # Number of features for SHAP
-    nb_features = settings_form.number_input(
-        'Nombre de variables (saisie)', 3, len(list(df))-1, 5)
-    nb_features_shap = settings_form.number_input(
-        'Nombre de variables (détails)', 1, len(list(df))-1, 20)
-    settings_form_submit_button = settings_form.form_submit_button('ok')
-
-    # Applicants features
-    st.sidebar.write('# Infos Client')
-    features_form = st.sidebar.form(key='features_form')
-
-    df_comp = df_filtred.copy()
-    df_comp['Clients'] = df_comp.SK_ID_CURR.apply(
-        lambda x: 'Sélectionné' if x == id else 'Autres')
-    df_comp = df_comp.sort_values(by='Clients')
-
-    applicant = df[df.SK_ID_CURR == id]
-    for i in range(nb_features):
-        feature = df.drop(
-            columns='SK_ID_CURR').columns.tolist()[i]
-        min = df[feature].min()
-        max = df[feature].max()
-        if max == 1:
-            value = int(applicant[feature].values[0])
-        else:
-            value = float(applicant[feature].values[0])
-        applicant.loc[applicant.SK_ID_CURR == id, feature] = features_form.number_input(
-            feature, min, max, value)
-    features_form_submit_button = features_form.form_submit_button('ok')
+            # Number of features for SHAP
+            nb_features = st.number_input(
+                'Nombre de variables (saisie)', 3, len(list(df))-1, 5)
+            nb_features_shap = st.number_input(
+                'Nombre de variables (détails)', 1, len(list(df))-1, 20)
+            settings_form_submit_button = st.form_submit_button('ok')
 
     if 'load_prediction' not in st.session_state:
         st.session_state.load_prediction = False
-    if features_form_submit_button or settings_form_submit_button or st.session_state.load_prediction:
+
+    if settings_form_submit_button or st.session_state.load_prediction:
         st.session_state.load_prediction = True
+
+        # Applicants features
+        st.write('## Client Sélectionné')
+        with st.expander("Infos client"):
+            # st.write('# Infos Client')
+            with st.form(key='features_form'):
+                df_comp = df_filtred.copy()
+                df_comp['Clients'] = df_comp.SK_ID_CURR.apply(
+                    lambda x: 'Sélectionné' if x == id else 'Autres')
+                df_comp = df_comp.sort_values(by='Clients')
+                applicant = df[df.SK_ID_CURR == id]
+                for i in range(nb_features):
+                    feature = df.drop(
+                        columns='SK_ID_CURR').columns.tolist()[i]
+                    min = df[feature].min()
+                    max = df[feature].max()
+                    if max == 1:
+                        value = int(applicant[feature].values[0])
+                    else:
+                        value = float(applicant[feature].values[0])
+                    applicant.loc[applicant.SK_ID_CURR == id, feature] = st.number_input(
+                        feature, min, max, value)
+                features_form_submit_button = st.form_submit_button('ok')
+
         await applicant_data(applicant)
         compare_viz(df_comp)
         await prediction(applicant, int(
             nb_features_shap), explainer, model)
-
-
-@cached(ttl=None, cache=Cache.MEMORY)
-async def get_data():
-    async with aiohttp.ClientSession() as session:
-        data = await fetch(session, dashboard_url+'data')
-        df = pd.DataFrame.from_dict(data, orient='tight')
-        for col in list(df):
-            if df[col].max() == 1:
-                df[col] = df[col].astype(int)
-        return df
-
-
-@cached(ttl=None, cache=Cache.MEMORY)
-async def get_model():
-    async with aiohttp.ClientSession() as session:
-        model = await fetch(session, dashboard_url+'model')
-        if model:
-            model = dillDecode(model)
-        else:
-            st.error(model)
-        return model
+# ========================================================================================================================
 
 if __name__ == '__main__':
     # Get data
